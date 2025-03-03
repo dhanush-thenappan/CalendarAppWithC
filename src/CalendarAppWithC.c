3e8d439a5909e64d9d55ea1ba80a73246d7fa184
Dhanush Thenappan <dhanushthenappan@gmail.com>
2025-03-03 20:44:43 +0530

    Test dayOfYear() function

--- src/CalendarAppWithC.c
+++ src/CalendarAppWithC.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "CalendarAppWithC.h"
+#include "DayOfDateCalculator.h"
+
 
 int main(void) {
+    int day, month, year;
+    char days[7][10] = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    printf("Enter date to find day (dd-mm-yyyy) : ");
+    scanf("%d-%d-%d", &day, &month, &year);
+    printf("%s\n",days[dayOfYear(day, month, year)]);
     return 0;
 }
