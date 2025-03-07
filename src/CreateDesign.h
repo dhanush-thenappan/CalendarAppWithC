194fce99d665d52b6a6cd3c0875743c73c342c0f
Dhanush Thenappan <dhanushthenappan@gmail.com>
2025-03-07 22:48:49 +0530

    Create function to design the calendar in console

---
+++ src/CreateDesign.h
@@ -0,0 +1,18 @@
+// Header guard
+#ifndef CREATEDESIGN_H
+
+// Define CREATEDESIGN_H
+#define CREATEDESIGN_H
+
+// Preprocessor directives
+#define MAX_ROW_CHAR_LIMIT 93
+#define MAX_MONTH_CHAR_LIMIT 29
+
+// Function declarations
+int PrintMonthName(int *monthCount);
+void CreateDesign();
+
+// Global variable declarations
+
+
+#endif
