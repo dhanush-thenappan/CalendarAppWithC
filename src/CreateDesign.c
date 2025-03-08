c7e05e40c5fe07d9b6db8c390194e66e513fe08c
Dhanush Thenappan <dhanushthenappan@gmail.com>
2025-03-08 20:32:16 +0530

    Add Comments to CreateDesign files

--- src/CreateDesign.c
+++ src/CreateDesign.c
@@ -1,42 +1,66 @@
+// Standard includes
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
 
+// User defined includes
 #include "CalendarAppWithC.h"
 #include "CreateDesign.h"
 #include "DayOfDateCalculator.h"
 
+// Global variable definitions
+
+// Name of all months
 char Months[12][10] = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", 
                        "MAY", "JUNE", "JULY", "AUGUST", 
                        "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
 
+// First letter of all days of the week, starting from Sunday 
 char Days[7] = {'S', 'M', 'T', 'W', 'T', 'F', 'S'};
 
+// Number of days in each month of the year. If leap year, February's number of days becomes 29 from main function.
 int DaysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+// Functions
+
+/*
+Function name: CreateDesign()
+Return type: void
+Parameters: int year, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates
+Process: Creates the calendar design. 
+         Based on the MAX_MONTH_LIMIT_PER_ROW set by the user in CreateDesign.h, this function calls
+         1. PrintMonthName() to print month names
+         2. PrintWeekRow() to print the week names
+         3. PrintSeparator() to print the separator line
+         4. PrintDates() to print all the dates in the months in that respective row. 
+*/
 void CreateDesign(int year, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates) {
-    int monthCount = 0;
-    int currentYear = FALSE;
-    if(year == Today.tm_year){
-        currentYear = TRUE;
-    }
-    for(int i = 0; i < TOTAL_MONTHS / MAX_MONTH_LIMIT_PER_ROW; i++, printf("\n\n")) {
+    int monthCount = 0; // Track of month name that was last printed
+    for(int i = 0; i < TOTAL_MONTHS / MAX_MONTH_LIMIT_PER_ROW; i++, printf("\n")) {
             PrintMonthName(&monthCount);
             printf("\n");
             PrintWeekRow();
             printf("\n");
             PrintSeparator();
             printf("\n");
-            PrintDates(year, i, currentYear, Today, birthDatesArray, sizeOfbirthDates);
+            PrintDates(year, i, Today, birthDatesArray, sizeOfbirthDates);
             printf("\n");
     }
 }
 
+/*
+Function name: PrintMonthName()
+Return type: void
+Parameters: int *monthCount
+Process: Prints the first line in each month calendar which is the month name. Gets the name from the global variable Months[][]
+         Uses the monthCount passed from the caller to keep track of current printing month
+*/
 void PrintMonthName(int *monthCount) {
     int monthLength;
     int charCount;
     int minSpace;
+    // Print the month names based on the limit per row set in CreateDesign.h
     for(int j = 0; j < MAX_MONTH_LIMIT_PER_ROW; j++, printf("   "), (*monthCount)++) {
         monthLength = strlen(Months[*monthCount]);
         charCount = 0;
@@ -58,7 +82,12 @@ void PrintMonthName(int *monthCount) {
     }
 }
 
-
+/*
+Function name: PrintWeekRow() 
+Return type: void
+Parameters: Nil
+Process: Prints the week names by their first letter starting from Sunday. The data is got from the Global variable Days[].
+*/
 void PrintWeekRow() {
     for(int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; i++, printf("   ")) {
         printf(CYAN "|" RESET);
@@ -68,6 +97,12 @@ void PrintWeekRow() {
     }
 }
 
+/*
+Function name: PrintSeparator() 
+Return type: void
+Parameters: Nil
+Process: Prints the separator line which is '---' to make a table look.
+*/
 void PrintSeparator() {
     for(int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; i++, printf("   ")) {
         printf(CYAN "|" RESET);
@@ -77,6 +112,12 @@ void PrintSeparator() {
     }
 }
 
+/*
+Function name: PrintRowEnd()
+Return type: void
+Parameters: Nil
+Process: Prints a separator line to at the end of each month to make it look seamless.
+*/
 void PrintRowEnd() {
     for(int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; i++, printf("   ")) {
         printf(CYAN "-" RESET);
@@ -86,15 +127,25 @@ void PrintRowEnd() {
     }
 }
 
-void PrintDates(int year, int startMonth, int currentYear, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates) { // 2025 3
-    int DayCount[MAX_MONTH_LIMIT_PER_ROW];
-    int over = 0;
-    int startDays[MAX_MONTH_LIMIT_PER_ROW];
-    int currentMonth = 0;
-    struct tm temp;
-    for(int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; DayCount[i++] = 1);
+/*
+Function name: PrintDates()
+Return type: void
+Parameters: int year, int startMonth, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates
+Process: This function prints the dates of each month in each row. This function updates all the months row by row, not month by month.
+         It also checks whether the year entered by the user is the current year. If yes, it prints Today's date in RED.
+         It also takes the BirthDates array from main function and checks whether any birth date is available in the current year. If found, it prints them in MAGENTA.
+*/
+void PrintDates(int year, int startMonth, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates) {
+    int DayCount[MAX_MONTH_LIMIT_PER_ROW]; // Current printing date of each month in the row
+    int over = 0; // To check if all months have been printed
+    int startDays[MAX_MONTH_LIMIT_PER_ROW]; // Finds the starting day of the month. That is the corrosponding day for the 1st of month.
+    int currentMonth = 0; 
+    struct tm currentWorkingDate;
+    for(int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; DayCount[i++] = 1); // Initialize all the start dates to 1
     for(int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; i++)
-        startDays[i] = dayOfDate(1,(startMonth * MAX_MONTH_LIMIT_PER_ROW)+1+i,year);
+        // startMonth * MAX_MONTH_LIMIT_PER_ROW)+1+i -> Gets the current working month
+        startDays[i] = dayOfDate(1,(startMonth * MAX_MONTH_LIMIT_PER_ROW)+1+i,year); // Find starting days of all months
+    // Since 0 in dayOfDate() is Saturday, changing it to 6 in the calendar.
     for (int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; i++) {
         if(startDays[i] == 0) {
             startDays[i] = 6;
@@ -103,58 +154,86 @@ void PrintDates(int year, int startMonth, int currentYear, struct tm Today, cons
             startDays[i] = startDays[i] - 1;
         }
     }
+    // Loop to print all the dates in the number of months specified in MAX_MONTH_LIMIT_PER_ROW.
+    // over != (int)(pow(2, MAX_MONTH_LIMIT_PER_ROW) - 1) -> Uses bits in the integer over to mark end of month. 
+    // For example, if MAX_MONTH_LIMIT_PER_ROW is set to 4, over should be 15 to make the loop stop.
+    // 15 in binary is 1111. When each month prints all its dates, it toggles the respective bit to one marking its completion.
     while (over != (int)(pow(2, MAX_MONTH_LIMIT_PER_ROW) - 1)) {
         printf(CYAN "|" RESET);
+        // Print for all the months in the row which is equal to MAX_MONTH_LIMIT_PER_ROW.
         for(int i = 0; i < MAX_MONTH_LIMIT_PER_ROW; i++) {
+            // Set the current month
             currentMonth = startMonth*MAX_MONTH_LIMIT_PER_ROW+i;
+            // Print dates in the respective days
             for(int j = 0; j < MAX_DAYS_IN_A_WEEK; j++, printf(CYAN "|" RESET)) {
-                temp.tm_mday = DayCount[i];
-                temp.tm_mon = currentMonth;
-                temp.tm_year = year;
+                // Update the currentWorkingDate with the day, month, year.
+                currentWorkingDate.tm_mday = DayCount[i];
+                currentWorkingDate.tm_mon = currentMonth;
+                currentWorkingDate.tm_year = year;
+                // If Day count in a particular month is completed, its bit position in "over" is toggled to 1. 
                 if (DayCount[i] > DaysInMonth[currentMonth]) {
                     over |= (int)pow(2, i);
                     printf("   ");
                 }
+                // If Day Count is not matching with end date of the month, else case is executed
                 else {
+                    // Finds the first day of the month using the startDays[] array and the loop counter.
                     if(j == startDays[i]) {
-                        
+                        // If the first day is the start of the month, then change the date color to RED. 
                         if(DayCount[i] == Today.tm_mday && currentMonth == Today.tm_mon && year == Today.tm_year)
                             printf(RED " %-2d" RESET, (DayCount[i])++);
-                        else if (CheckValidBirthDate(birthDatesArray, sizeOfbirthDates, temp))
+                        // If first day is not today, then check if it is a birth date. If yes, then change the date color to MAGENTA
+                        else if (CheckValidBirthDate(birthDatesArray, sizeOfbirthDates, currentWorkingDate))
                             printf(MAGENTA " %-2d" RESET, (DayCount[i])++);
+                        // Else print the date in GREEN color.
                         else
                             printf(GREEN " %-2d" RESET, (DayCount[i])++);
                     }
+                    // If not the first day, and the first day is already printed, then enter this case
                     else if(DayCount[i] > 1) {
+                        // If the current day is the start of the month, then change the date color to RED. 
                         if(DayCount[i] == Today.tm_mday && currentMonth == Today.tm_mon && year == Today.tm_year)
                             printf(RED " %-2d" RESET, (DayCount[i])++);
-                        else if (CheckValidBirthDate(birthDatesArray, sizeOfbirthDates, temp))
+                        // If current day is not today, then check if it is a birth date. If yes, then change the date color to MAGENTA
+                        else if (CheckValidBirthDate(birthDatesArray, sizeOfbirthDates, currentWorkingDate))
                             printf(MAGENTA " %-2d" RESET, (DayCount[i])++);
+                        // Else print the date in GREEN color.
                         else
                             printf(GREEN " %-2d" RESET, (DayCount[i])++);
                     }
+                    // If not the valid day, then print empty spaces
                     else printf("   ");
                 }
             }
+            // Print space and | when the current month is not the last in the row.
             if (i != MAX_MONTH_LIMIT_PER_ROW - 1) {
                 printf("   ");
                 printf(CYAN "|" RESET);    
             }
         }
         printf("\n");
+        // If the month is not over print the separator by calling PrintSeparator().
         if(over != (int)(pow(2, MAX_MONTH_LIMIT_PER_ROW) - 1))
             PrintSeparator();
+        // If month is over, then call the PrintRowEnd() to print a end line.
         else
             PrintRowEnd();
         printf("\n");
     }
 }
 
+/*
+Function name: CheckValidBirthDate()
+Return type: int
+Parameters: const struct tm birthDatesArray[], int sizeOfbirthDates, struct tm checkDate
+Process: Loops through the birthDatesArray[] and checks whether it matches the checkDate. If mathces, the function returns 1.
+*/
 int CheckValidBirthDate(const struct tm birthDatesArray[], int sizeOfbirthDates, struct tm checkDate) {
     for (int i = 0; i < sizeOfbirthDates; i++) {
-        if( birthDatesArray[i].tm_mday == checkDate.tm_mday && 
-            (birthDatesArray[i].tm_mon - 1) == checkDate.tm_mon && 
-            birthDatesArray[i].tm_year == checkDate.tm_year) {
+        // Check the year, month and date. If all 3 matches, return 1.
+        if( birthDatesArray[i].tm_year == checkDate.tm_year &&
+            (birthDatesArray[i].tm_mon - 1) == checkDate.tm_mon &&
+            birthDatesArray[i].tm_mday == checkDate.tm_mday) {
                 return 1;
         }
     }
--- src/CreateDesign.h
+++ src/CreateDesign.h
@@ -5,25 +5,28 @@
 #define CREATEDESIGN_H
 
 // Preprocessor definitions
-#define MAX_MONTH_CHAR_LIMIT 29
-#define MAX_MONTH_LIMIT_PER_ROW 6
-#define TOTAL_MONTHS 12
-#define MAX_DAYS_IN_A_WEEK 7
-#define TRUE 1
-#define FALSE 0
-#define RESET   "\x1B[0m"    // Reset color
-#define RED     "\x1B[31m"   // Red color (for today)
-#define GREEN   "\x1B[32m" // Green
-#define YELLOW  "\x1B[33m" // Yellow
-#define CYAN    "\x1B[36m" // Cyan (Light Blue)
-#define BLUE    "\x1B[34m" // Blue
-#define MAGENTA "\x1B[35m" // Magenta (Purple)
+#define MAX_MONTH_CHAR_LIMIT 29 // Maximum limit (width) of characters for each month in the calendar
+#define MAX_MONTH_LIMIT_PER_ROW 6 // Maximum months in a single row of the calendar
+#define TOTAL_MONTHS 12 // Total months in a year
+#define MAX_DAYS_IN_A_WEEK 7 // Total days in a week
+#define TRUE 1 // TRUE means 1
+#define FALSE 0 // False means 0
+
+// ANSI escape codes for colors
+#define RESET   "\x1B[0m"  // Reset color
+#define RED     "\x1B[31m" // Red color (for today)
+#define GREEN   "\x1B[32m" // Green (for day names)
+#define YELLOW  "\x1B[33m" // Yellow (for week names)
+#define CYAN    "\x1B[36m" // Cyan (for box lines)
+#define BLUE    "\x1B[34m" // Blue (for month names)
+#define MAGENTA "\x1B[35m" // Magenta (for birth dates)
+
 // Function declarations
 void CreateDesign(int year, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates);
 void PrintMonthName(int *monthCount);
 void PrintWeekRow();
 void PrintSeparator();
-void PrintDates(int year, int startMonth, int currentYear, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates);
+void PrintDates(int year, int startMonth, struct tm Today, const struct tm birthDatesArray[], int sizeOfbirthDates);
 void PrintRowEnd();
 int CheckValidBirthDate(const struct tm birthDatesArray[], int sizeOfbirthDates, struct tm checkDate);
 
